0bea3117be4a6e0df29e6ac4f88ac2e2b2340b27
Raul <raulvirtuatech@gmail.com>
2024-07-31 11:22:57 -0300

    Criando repositorio e util para formatacao SQL

---
+++ src/main/java/com/virtuatech/gestao_de_treinamentos/modelo/repositorio/CrudRepositorio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.virtuatech.gestao_de_treinamentos.modelo.repositorio;
+
+/**
+ *
+ * @author Renato
+ */
+public interface CrudRepositorio {
+    
+}
